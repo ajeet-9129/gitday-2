4e1be7fb65be9d8dcc0775980322cd918d7b40da
ajeet-9129 <ajeetkumar.04305@gmail.com>
2025-04-09 00:43:28 +0530

    new app.py file is created

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+# Create the Flask app
+app = Flask(__name__)
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Result route to handle form submission
+@app.route('/result', methods=['POST'])
+def result():
+    # Get the value from the form input
+    name = request.form['name']
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    # Run the Flask app
+    app.run(debug=True)
+
